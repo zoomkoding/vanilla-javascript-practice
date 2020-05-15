c52f3453dd92d54e133f7c3b140985fb0cca164c
zoomkoding <gur5381@gmail.com>
2020-05-15 10:29:11 +0900

    problem init

--- src/Atoms/CheckBox.js
+++
@@ -1,8 +0,0 @@
-export default function CheckBox (onclick) {
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = false;
-    checkbox.onclick = () => onclick(checkbox.checked);
-    checkbox.style.margin = '0 10px'
-    return checkbox;
-}
--- src/Molecules/Section.js
+++
@@ -1,8 +0,0 @@
-import Title from './Title.js';
-
-export default function Section(id, title) {
-    const section = document.createElement('div');
-    section.id = id;
-    section.appendChild(Title(title)); 
-    return section;
-};
